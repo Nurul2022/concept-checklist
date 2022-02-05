cde190654e4af077fb17b608ca6a7c6eaeed37b6
Nurul2022 <nurulbdpersonal@gmail.com>
2022-02-05 10:23:41 +0600

    let const a2 added

--- concept-checklist/a2.js
+++ concept-checklist/a2.js
@@ -1,1 +1,11 @@
 // 2. 2 variables using (let, const)
+
+// value of variable could change 
+let price = 27; 
+price = 29;
+price = 31;
+console.log(price);
+
+// value of the variable will not change 
+const myName = 'Nurul Islam';
+console.log(myName);
